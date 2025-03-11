42d37347a7eb9c12fe34c633f202511fb0a7de85
unknown <lukas.smetana@infis.cz>
2025-03-11 08:36:49 +0100

    poznámky 1

---
+++ poznamky.py
@@ -0,0 +1,16 @@
+#nacteni od uzivatele - pokud potřebujeme něco zjistit od uživatele
+input("")
+#přetypování - pokud chceme třeba z textový proměnný číslo, aby to byl integer
+cislo=int(input(""))
+#podminky - pokud potřebujeme vymazat nějaké možnosti, tak python se podívá na podmínku a elif pokud něco bude splňovat vypíše danou větua pokud ne vypíše else
+#vypisování - pokud chceme něco vypsat použijeme print a závorku a v ní uvozovky do které vložíme text
+#logické operce - pokud je někde and tak to spojuje 2 věci na které přihlíží
+if cislo<10 and cislo>1:
+    print("...")
+elif cislo>16:
+    print("...")
+else:
+    print("...")
+#github příkaz 1 - git add *
+#github příkaz 2 - git commit -m "text"
+#github příkaz 3 - git push
